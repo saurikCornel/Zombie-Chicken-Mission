665d419c6c731644e67696fbad2f96f92af1c81f
Alex Belov <alex@cornel.su>
2025-03-24 20:58:33 +0300

    Initial Commit

---
+++ Zombie Chicken Mission/Zombie_Chicken_MissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zombie_Chicken_MissionApp.swift
+//  Zombie Chicken Mission
+//
+//  Created by alex on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Zombie_Chicken_MissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Zombie Chicken MissionTests/Zombie_Chicken_MissionTests.swift
@@ -0,0 +1,17 @@
+//
+//  Zombie_Chicken_MissionTests.swift
+//  Zombie Chicken MissionTests
+//
+//  Created by alex on 3/24/25.
+//
+
+import Testing
+@testable import Zombie_Chicken_Mission
+
+struct Zombie_Chicken_MissionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
